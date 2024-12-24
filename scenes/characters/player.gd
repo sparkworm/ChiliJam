2db44e6290aa14afc9fdf57edd426d1682e5dc49
Sparkworm <d.clutter2337@gmail.com>
2024-12-23 19:59:08 -0600

    Added movement to player

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -2,13 +2,14 @@ class_name Player
 extends CharacterBody2D
 
 @export var max_look_angle: float = PI/4
+@export var speed: float = 50.0
 
 @onready var body: Sprite2D = $Body
 @onready var hat: Sprite2D = $Hat
 
 func _process(delta: float) -> void:
+	handle_movement()
 	# make the hat look in the direction of the mouse
-	# NOTE: should only be done when the player isn't moving
 	hat.rotation = get_angle_to(get_global_mouse_position()) + PI/2
 	if abs(angle_difference(hat.rotation, body.rotation)) > max_look_angle:
 		if abs(angle_difference(hat.rotation + max_look_angle, body.rotation)) < \
@@ -16,3 +17,8 @@ func _process(delta: float) -> void:
 			body.rotation = hat.rotation + max_look_angle
 		else:
 			body.rotation = hat.rotation - max_look_angle
+
+func handle_movement() -> void:
+	var movement_vec: Vector2 = Input.get_vector("left", "right", "up", "down")
+	velocity = movement_vec * speed
+	move_and_slide()
